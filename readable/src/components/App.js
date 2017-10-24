939a39d6c1262dbaeea844ecf29eeb723e824810
Manoj Talagadadeevi <manojt.854@gmail.com>
2017-10-24 16:04:50 -0400

    Create App component and removing the default one

---
+++ readable/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class App extends Component {
+    state = {
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <div className='nav'>
+                    <h1 className='header'>Readable</h1>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default App
