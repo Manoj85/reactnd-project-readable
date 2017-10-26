ee3c38ccfb24c01b0df2e1ebc8a0acf9e67a4d64
Manoj Talagadadeevi <manojt.854@gmail.com>
2017-10-26 11:36:35 -0400

    Simplify Category List display

--- readable/src/components/category/CategoryList.js
+++ readable/src/components/category/CategoryList.js
@@ -15,23 +15,12 @@ class CategoryList extends Component {
 		return(
 			<section className="col-sm-4">
 				<h1>Categories</h1>
-				{(this.props.categories || []).map(category =>
-					<div>
-						<p key={category.name}>
-							<Link to= {`/${category.name}`}
-							      onClick={(e) => this.props.selectedCategory(category.name)}> {category.name}</Link>
-						</p>
-					</div>
-				)
-				}
-				<p>
-					<Link to="/"
-					      onClick={(e) => this.props.selectedCategory("all")}>Show all Categories Posts</Link>
-				</p>
+				{_.map(this.props.categories,(category) =>{
+					return <p><Link key={category.name} to={`/categories/${category.path}`}>{category.name}</Link></p>
+				})}
 			</section>
 		)
 	}
-
 }
 
 function mapStateToProps({categories}) {
