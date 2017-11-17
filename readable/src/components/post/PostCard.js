100e972d987ea91c4f1a749fada329fc2d8df81c
Manoj Talagadadeevi <manojt.854@gmail.com>
2017-11-17 14:50:22 -0500

    PostCard Component: [1] Hyperlink the Post Title [2] Adding "Edit" and "Delete" icons

--- readable/src/components/post/PostCard.js
+++ readable/src/components/post/PostCard.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import EditIcon from 'react-icons/lib/fa/edit'
+import DeleteIcon from 'react-icons/lib/md/delete'
 
 class PostCard extends Component {
     render() {
@@ -6,7 +10,22 @@ class PostCard extends Component {
         return (
             <div class="card margin-top-10" key={post.id}>
                 <div class="card-body">
-                    <h4 class="card-title">{post.title}</h4>
+                    <h5 class="card-title">
+                        <div className="row">
+                            <div className="col-md-8">
+                                <Link to={'/' + post.category + '/' + post.id}> { post.title } </Link>
+                                <span className="text-muted" style={{fontSize: 16}}>{post.timestamp}</span>
+                            </div>
+                            <div className="col-md-2 ml-md-auto">
+                                <button className="btn btn-info btn-sm margin-left-15" id={'edit' + post.id} onClick={this.editPost}>
+                                    <EditIcon size={20}/>
+                                </button>
+                                <button className="btn btn-danger btn-sm margin-left-15" id={'delete' + post.id} onClick={this.deletePost}>
+                                    <DeleteIcon size={20}/>
+                                </button>
+                            </div>
+                        </div>
+                    </h5>
                     <h6 class="card-subtitle mb-2 text-muted">By: {post.author}</h6>
                     <p class="card-text"></p>
                 </div>
